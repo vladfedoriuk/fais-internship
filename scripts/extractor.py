7b9bebc964eedeb03be1291c4ce1f8469ffd1de7
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2021-03-24 23:34:23 +0200

    refactored extractor.py

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from sqlalchemy.sql import select, join
 from functools import reduce
 import logging
+import pandas
 import sys
 import re
 
@@ -15,7 +16,105 @@ factory = ConfTableFactory()
 tables_query = 'SELECT table_name FROM information_schema.tables ' + \
     f'WHERE table_name NOT IN ("configuration", "files") AND ' + \
         f'table_schema = \"{DB["database"]}\";'
+ 
+ 
+def validate_date(date):
+    for pattern in time_patterns_set:
+        try:
+            valid_date = datetime.strptime(date, pattern)
+            return valid_date
+        except ValueError:
+            pass 
+    else:
+        logging.error('No time pattern matched for validfrom. Use -h flag to see the whole pattern')
+        sys.exit()
+
+
+def process_run(tables, args):
+    
+    try:
+        run_id = int(args.run)
+    except ValueError:
+        logging.error('The run id shoud be a positive integer')
+        sys.exit()
+    
+    versions = {}
+    for table_name, table in tables.items():
+        j = join(
+            Files, table, 
+            (Files.c.start_time >= table.c.valid_from) & \
+                (Files.c.stop_time <= table.c.valid_to)
+        )
+        results = connection.execute(
+            select(
+                table.c.valid_from, 
+                table.c.valid_to, 
+                table.c.version, 
+                table.c.remarks
+            ).select_from(j).where(Files.c.run_id == run_id)
+        ).fetchall()
+        versions[table_name] = set(res for res in results)
+    
+    available_versions = reduce(lambda s1, s2: s1 & s2, versions.values())
+    available_versions = list(available_versions)
+    
+    df = pandas.DataFrame(
+        data=available_versions, 
+        index=range(len(available_versions)),
+        columns=['VALID_FROM', 'VALID_TO', 'VERSION', 'REMARKS']
+    )
+    print(df)
+    df.index.name = 'ID'
+
+    idx = int(input('Input a version ID:'))
+    
+    if not all(available_versions[idx] in vers for vers in versions.values()):
+        logging.error('The version in not present in some of the configurations.')
+        sys.exit()  
         
+    valid_from = available_versions[idx][0]
+    valid_to = available_versions[idx][1]
+    version = available_versions[idx][2]
+    
+    return valid_from, valid_to, version
+ 
+ 
+def process_dates(tables, args):
+    versions = {}
+    for table_name, table in tables.items():
+        query = select(table.c.version, table.c.remarks)\
+            .where(table.c.valid_from <= valid_from, \
+                table.c.valid_to >= valid_to
+            )
+        results = connection.execute(query).fetchall()
+        versions[table_name] = frozenset(
+            res for res in results
+        )
+        
+    available_versions = reduce(lambda s1, s2: s1 & s2, versions.values())
+    available_versions = list(available_versions)
+    
+    if not args.version:
+        print(f'available verions of configurations:')
+        df = pandas.DataFrame(
+            data=available_versions,
+            index=range(len(available_versions)),
+            columns=['VERSION', 'REMARKS']
+        )
+        df.index.name = 'ID'
+        print(df)   
+        version_id = int(input('Please choose one [input an index]:'))
+        version = available_versions[version_id]
+        
+    else:
+        version = args.version
+    
+    if not all(version in vers for vers in versions.values()):
+        logging.error('The version is not present in some of the configurations.')
+        sys.exit()  
+    
+    return version     
+
 
 if __name__ == '__main__':
     
@@ -32,98 +131,23 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     
-    if args.validfrom:
-        for pattern in time_patterns_set:
-            try:
-                valid_from = datetime.strptime(args.validfrom, pattern)
-                break
-            except ValueError:
-                pass 
-        else:
-            logging.error('No time pattern matched for validfrom. Use -h flag to see the whole pattern')
-            sys.exit()
-    
-    if args.validto:   
-        for pattern in time_patterns_set:
-            try:
-                valid_to = datetime.strptime(args.validto, pattern)
-                break
-            except ValueError:
-                pass
-        else:
-            logging.error('No time pattern matched for validto. Use -h flag to see the whole pattern')
-            sys.exit()
+    if args.validfrom is not None:
+        valid_from = validate_date(args.validfrom)
+        
+    if args.validto is not None:   
+        valid_to = validate_date(args.validto)
     
     table_names = connection.execute(tables_query)
     tables = {}
     for table_name in table_names:
         tables[table_name[0]] = factory(table_name[0])
         
-    if args.run:
-        
-        versions = set()
-        for table_name, table in tables.items():
-            j = join(
-                Files, table, 
-                (Files.c.start_time >= table.c.valid_from) & \
-                    (Files.c.stop_time <= table.c.valid_to)
-            )
-            results = connection.execute(
-                select(
-                    table.c.valid_from, 
-                    table.c.valid_to, 
-                    table.c.version, 
-                    table.c.remarks
-                ).select_from(j)
-            ).fetchall()
-            versions |= set(res for res in results)
-            
-        versions = list(versions)
-        print('ID\t\tVALID_FROM\t\t\t\tVALID_TO\tVERSION\t\t\tREMARKS') 
-        for i, version in enumerate(versions):
-            print(f"{i}\t\t{str(version[0])}\t\t{str(version[1])}\t\t{version[2]}\t{version[3] if version[3] else ''}")
-        idx = int(input('Input a version ID:'))
-        
-        valid_from = versions[idx][0]
-        valid_to = versions[idx][1]
-        version = versions[idx][2]
-        
+    if args.run is not None:
+        valid_from, valid_to, version = process_run(tables, args)
     else:
+        version = process_dates(tables, args)
         
-        versions = {}
-        for table_name, table in tables.items():
-            query = select(table.c.version, table.c.remarks)\
-                .where(table.c.valid_from <= valid_from, \
-                    table.c.valid_to >= valid_to
-                )
-            results = connection.execute(query).fetchall()
-            versions[table_name] = frozenset(
-                (res[0], res[1]) if res[1] else res[0] for res in results
-            )
-        
-        available_versions = reduce(lambda s1, s2: s1 & s2, versions.values())
-    
-        if not args.version:
-        
-            print(f'available verions of configurations:')
-            print('VERSION\t\tREMARKS')
-            for v in sorted(available_versions, key=lambda x: x if isinstance(x, int) else x[0]):
-                if isinstance(v, tuple):
-                    print(f"{v[0]}\t\t{v[1]}")
-                else:
-                    print(v)
-                
-            version = int(input('Please choose one [input a number]:'))
-        else:
-            version = args.version
-    
-        if not all(version in vers for vers in versions.values()):
-            logging.error('The version in not present in some of the configurations.')
-            sys.exit()
-     
-     
-       
-    if not args.name:
+    if args.name is None:
         file_name = f'./configuration_{valid_from}_{valid_to}_version_{version}'
     else:
         file_name = args.name
