ea279192e456b363cc74e6dcfadc22c4ca4fe5c2
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2021-03-26 18:46:39 +0200

    updated requirements, started working on webgui

--- web/webgui/webgui/settings.py
+++ web/webgui/webgui/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # webgui
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
+    'interact.apps.InteractConfig'
 ]
 
 MIDDLEWARE = [
