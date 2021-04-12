42cddbd9ba85d43826107253fcc5c3b86493e359
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2021-04-12 13:35:49 +0300

    refactored parser.py

--- scripts/parser.py
+++ scripts/parser.py
@@ -5,9 +5,12 @@ import argparse
 from dataclasses import dataclass
 from collections import defaultdict
 from datetime import datetime, timedelta
-from utils.db import ConfTableFactory, Files, engine, tables_query
+from utils.db import ConfTableFactory, Files, engine, tables_query, EXCLUDE
 from utils.time import time_patterns_set, r_time_patterns, validate_date
 from sqlalchemy.sql import select
+from itertools import chain
+from functools import singledispatchmethod
+from typing import Tuple
 
 
 parser = argparse.ArgumentParser()
@@ -18,6 +21,16 @@ connection = engine.connect()
 class Token:
     kind: str
     value: str
+    
+@dataclass
+class Filenames:
+    filename_from: str
+    filename_to: str
+    
+@dataclass
+class Runs:
+    run_from: int
+    run_to: int
 
 class Tokenizer(object):
     
@@ -69,8 +82,13 @@ class Converter(object):
             connection.execute(ins)   
                      
 
-def write_to_database(conf_file, version, valid_from, valid_to, remarks=None):
-    with open(conf_file) as file:
+def write_to_database(conf_file_path, version, valid_from, valid_to, remarks=None):
+    table_names = connection.execute(tables_query)
+    table_names = set(x for x in chain.from_iterable(table_names) if \
+        not any(re.match(y, x) for y in EXCLUDE)
+    )
+    seen_tables = set()
+    with open(conf_file_path) as file:
         tokenizer = Tokenizer(file.read())
         converter = Converter()
         current_table = None
@@ -79,10 +97,74 @@ def write_to_database(conf_file, version, valid_from, valid_to, remarks=None):
                 current_table = token.value[
                     token.value.index('[') + 1 : token.value.index(']')
                 ]
+                if current_table not in table_names:
+                    raise ValueError(f'there is no such table in the database: {current_table}')
+                seen_tables.add(current_table)
             elif token.kind == 'PARAMS' and current_table:
                 converter[current_table] = token.value
+        if seen_tables != table_names:
+            raise ValueError(f'The configurations for tables {", ".join(table_names - seen_tables)} where not found')
         converter.save(version, valid_from, valid_to, remarks=remarks)
-            
+
+
+class ValidityDates(object):
+    
+    @singledispatchmethod
+    def from_params(self, arg) -> Tuple[datetime, datetime]:
+        raise NotImplementedError(f"Cannot extract valididity dates from {type(arg)}")
+         
+    @from_params.register
+    def _(self, arg: Runs) -> Tuple[datetime, datetime]:
+        valid_from = connection.execute(
+            select(Files.c.start_time).where(
+                Files.c.run_id==arg.run_from
+            )
+        ).fetchone()
+        
+        if not valid_from:
+            raise ValueError('incorrect run-from id (there is no such run in a database).')
+        
+        valid_from = valid_from[0]
+        
+        valid_to = connection.execute(
+            select(Files.c.start_time).where(
+                Files.c.run_id==arg.run_to
+            )
+        ).fetchone()
+        
+        if not valid_to:
+            raise ValueError('incorrect run-to id (there is no such run in a database).')
+        
+        valid_to = valid_to[0]
+        
+        return valid_from, valid_to
+        
+    
+    @from_params.register
+    def _(self, arg: Filenames) -> Tuple[datetime, datetime]:
+        valid_from = connection.execute(
+            select(Files.c.start_time).where(
+                Files.c.file_name==arg.filename_from
+            )
+        ).fetchone()
+        
+        if not valid_from:
+            raise ValueError('incorrect run-from filename (there is no such run in a database).')
+        
+        valid_from = valid_from[0]
+        
+        valid_to = connection.execute(
+            select(Files.c.start_time).where(
+                Files.c.file_name==arg.filename_to
+            )
+        ).fetchone()
+        
+        if not valid_to:
+            raise ValueError('incorrect run-to filename (there is no such run in a database).')
+        
+        valid_to = valid_to[0]
+        
+        return valid_from, valid_to          
             
 if __name__ == '__main__':
     
@@ -119,48 +201,43 @@ if __name__ == '__main__':
             raise ValueError()
     except ValueError:
         logging.error('Th version should be a positive integer')
-    
-    if args.runfrom is not None:
-        valid_from = connection.execute(
-            select(Files.c.start_time).where(
-                Files.c.run_id==args.runfrom
-            )
-        ).fetchone()[0]
-    
-    if args.runto is not None:
-        valid_to = connection.execute(
-            select(Files.c.start_time).where(
-                Files.c.run_id==args.runto
-            )
-        ).fetchone()[0]
-    
-    if args.filefrom is not None:
-        valid_from = connection.execute(
-            select(Files.c.start_time).where(
-                Files.c.file_name==args.filefrom
-            )
-        ).fetchone()[0] 
-    
-    if args.fileto is not None:
-        valid_to = connection.execute(
-            select(Files.c.start_time).where(
-                Files.c.file_name==args.fileto
-            )
-        ).fetchone()[0]  
         
-    if args.validfrom and not valid_from:
+    if args.validfrom:
         valid_from = validate_date(args.validfrom, 'validfrom')
     
-    if args.validto and not valid_to:
+    if args.validto:
         valid_to = validate_date(args.valid_to, 'validto')
-        
-    write_to_database(
-        conf_file=conf_file,
-        version=version,
-        valid_from=valid_from,
-        valid_to=valid_to,
-        remarks=args.remarks
-    )
+    
+    if args.runfrom and args.runto:
+        arg = Runs(args.runfrom, args.runto)
+    elif args.filefrom and args.fileto:
+        arg = Filenames(args.filefrom, args.fileto)
+    
+    if not valid_from and not valid_to:
+        vd = ValidityDates()
+        try:
+            valid_from, valid_to = vd.from_params(arg)
+        except Exception as e:
+            error = e.args
+            logging.error(error)
+            sys.exit()
+    
+    if valid_from and valid_to:
+        try:
+            write_to_database(
+                conf_file_path=conf_file,
+                version=version,
+                valid_from=valid_from,
+                valid_to=valid_to,
+                remarks=args.remarks
+            )
+        except Exception as e:
+            error = e.args
+            logging.error(error)
+            sys.exit()
+    else:
+        logging.error('Failed to extract the validity dates from provided parameters.')
+        sys.exit()
         
                 
     
